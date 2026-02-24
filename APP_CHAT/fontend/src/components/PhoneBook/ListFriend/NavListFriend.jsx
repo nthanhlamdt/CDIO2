341e3dc922f33f43287b51214d31c6f377de9d8d
Thành Lâm <126042321+nthanhlamdt@users.noreply.github.com>
2026-02-25 00:02:28 +0700

    Delete APP_CHAT directory

--- APP_CHAT/fontend/src/components/PhoneBook/ListFriend/NavListFriend.jsx
+++
@@ -1,9 +0,0 @@
-function NavListFriend() {
-  return (
-    <div className=" bg-emerald-500 px-5 py-3 flex items-center justify-between h-20 z-20">
-      Danh sách bạn bè
-    </div>
-  )
-}
-
-export default NavListFriend
